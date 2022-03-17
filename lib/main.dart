544949827673ddfcfcfbc4e2220ed2af575bcae0
AdheenaBabu <babu.adheena.2001@gmail.com>
2022-03-17 13:29:19 +0530

    Added Start Page, Login Page and SignUp page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:payment_app/screens/home_page.dart';
+import 'package:payment_app/screens/start_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData.light(),
       //TODO: change to login / signup page and send to home page if token found
-      home: const HomePage(),
+      home: const StartPage(),
     );
   }
 }

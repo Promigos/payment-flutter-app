1278c7084fa898392e2e125cd894583d350d92af
sooryasrajan <soorya.s27@gmail.com>
2022-03-25 11:09:27 +0530

    Added wallet icon

--- lib/screens/start_page.dart
+++ lib/screens/start_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:payment_app/screens/sign_up.dart';
-import 'package:payment_app/utils/colors.dart' as colors;
-import 'package:payment_app/utils/constants.dart' as constants;
 import 'package:payment_app/screens/login_page.dart';
-
 
 class StartPage extends StatefulWidget {
   const StartPage({Key? key}) : super(key: key);
@@ -16,35 +14,62 @@ class StartPage extends StatefulWidget {
 class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(child: Container(), flex: 8,),
-              Expanded(child: Text('Welcome to\nWALLET UP', style: GoogleFonts.nunito(fontSize:35,fontWeight: FontWeight.bold,),),flex: 4,),
-              SizedBox(height: 20,),
-              Expanded(child: Text('Wallet anywhere anytime!', style: TextStyle(),)),
-              Expanded(child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(style: ElevatedButton.styleFrom(),
-                    onPressed: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const SignUp()));
-                  }, child: Text('SIGN UP',)),
-                  const SizedBox(width: 20,),
-                  ElevatedButton(style: ElevatedButton.styleFrom(), onPressed: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const LoginPage()));
-                  }, child: Text('LOG IN', ))
-                ],
-              ), flex: 12,)
-            ],
-          ),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(child: Container()),
+            Text(
+              'Welcome to\nWALLET UP',
+              style: GoogleFonts.nunito(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              'Wallet anywhere anytime!',
+              style: TextStyle(),
+            ),
+            Expanded(child: Container()),
+            const Expanded(
+                child: Center(child: FaIcon(FontAwesomeIcons.wallet, size: 150,))),
+            Expanded(
+              child: Container(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const SignUp()));
+                    },
+                    child: const Text(
+                      'SIGN UP',
+                    )),
+                const SizedBox(
+                  width: 20,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const LoginPage()));
+                    },
+                    child: const Text(
+                      'LOG IN',
+                    ))
+              ],
+            )
+          ],
         ),
       ),
     );
   }
+
+  _StartPageState();
 }

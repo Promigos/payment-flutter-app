cb5573d0520aae1609d84777185038977c223629
AdheenaBabu <babu.adheena.2001@gmail.com>
2022-03-25 12:32:27 +0530

    Added Biometric Verification

--- android/app/src/main/kotlin/com/example/payment_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/payment_app/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.payment_app
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }

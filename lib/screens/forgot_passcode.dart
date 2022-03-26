d05e6c204bae0e593a6265ad1fa6aa502dc5c0dc
AdheenaBabu <babu.adheena.2001@gmail.com>
2022-03-26 10:48:01 +0530

    Added Forgot Password page

---
+++ lib/screens/forgot_passcode.dart
@@ -0,0 +1,143 @@
+import 'dart:convert';
+
+import 'package:payment_app/utils/http_modules.dart';
+import 'package:payment_app/widgets/error_box.dart';
+import 'package:payment_app/widgets/password_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:payment_app/utils/colors.dart' as colors;
+import 'package:payment_app/utils/constants.dart' as constants;
+import 'package:google_fonts/google_fonts.dart';
+import 'package:oktoast/oktoast.dart';
+
+class ForgotPasswordChange extends StatefulWidget {
+  const ForgotPasswordChange({Key? key, required this.email}) : super(key: key);
+  final String email;
+
+  @override
+  _ForgotPasswordChange createState() => _ForgotPasswordChange();
+}
+
+class _ForgotPasswordChange extends State<ForgotPasswordChange> {
+  String error = "";
+  final _formKey = GlobalKey<FormState>();
+  final _verificationKeyController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool showProgress = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Center(
+        child: Container(
+          height: double.maxFinite,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 80),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Enter verification key and new password to change your password',
+                      style: GoogleFonts.nunito(
+                        color: colors.primaryTextColor,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: constants.textFieldPadding,
+                  child: PasswordFormFieldWidget(
+                    hintText: 'Please enter your verification key',
+                    label: 'Verification Key',
+                    controller: _verificationKeyController,
+                    validator: (value) {
+                      if (value == "" || value == null) {
+                        return "Please enter verification key";
+                      } else {
+                        return null;
+                      }
+                    },
+                    style:
+                    GoogleFonts.montserrat(color: colors.primaryTextColor),
+                  ),
+                ),
+                Padding(
+                  padding: constants.textFieldPadding,
+                  child: PasswordFormFieldWidget(
+                    hintText: 'Please enter your password',
+                    label: 'Password',
+                    controller: _passwordController,
+                    validator: (value) {
+                      if (value == "" || value == null) {
+                        return "Please enter password";
+                      } else {
+                        return null;
+                      }
+                    },
+                    style:
+                    GoogleFonts.montserrat(color: colors.primaryTextColor),
+                  ),
+                ),
+                Padding(
+                  padding: constants.textFieldPadding,
+                  child: error == "" ? Container() : errorBox(error),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0, left: 20),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: showProgress
+                        ? const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: CircularProgressIndicator(),
+                    )
+                        : ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            showProgress = true;
+                          });
+                          var res = await makePostRequest(
+                              json.encode({
+                                "email": widget.email,
+                                "forgotPasswordCode":
+                                _verificationKeyController.text,
+                                "password": _passwordController.text
+                              }),
+                              "/forgotPasswordVerify",
+                              null,
+                              false);
+                          setState(() {
+                            showProgress = false;
+                          });
+                          if (res.statusCode == 200) {
+                            showToast("Password changed successfully!");
+                            Navigator.of(context).pop();
+                          } else {
+                            setState(() {
+                              error = json.decode(res.body)['message'];
+                            });
+                          }
+                        }
+                      },
+                      child: Text(
+                        'CHANGE PASSWORD',
+                        style: GoogleFonts.nunito(
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/password_widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:payment_app/utils/colors.dart' as colors;
+
+class PasswordFormFieldWidget extends StatefulWidget {
+  const PasswordFormFieldWidget(
+      {Key? key,
+        this.passwordKey,
+        this.controller,
+        this.style,
+        this.validator,
+        required this.label,
+        required this.hintText})
+      : super(key: key);
+  final GlobalKey<FormFieldState>? passwordKey;
+  final TextEditingController? controller;
+  final TextStyle? style;
+  final String? Function(String?)? validator;
+  final String label;
+  final String hintText;
+
+  @override
+  _PasswordFormFieldWidgetState createState() =>
+      _PasswordFormFieldWidgetState();
+}
+
+class _PasswordFormFieldWidgetState extends State<PasswordFormFieldWidget> {
+  bool setPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: !setPasswordVisible,
+      key: widget.passwordKey,
+      controller: widget.controller,
+      validator: widget.validator,
+      style: widget.style,
+      decoration: InputDecoration(
+        label: Text(widget.label,
+            style: GoogleFonts.nunito(
+                color: colors.textBoxTextColor, fontSize: 12)),
+        hintText: widget.hintText,
+        suffixIcon: Material(
+          color: Colors.transparent,
+          child: IconButton(
+            color: colors.primaryTextColor,
+            icon: Icon(
+              setPasswordVisible ? Icons.visibility : Icons.visibility_off,
+            ),
+            onPressed: () {
+              setState(() {
+                setPasswordVisible = !setPasswordVisible;
+              });
+            },
+          ),
+        ),
+        filled: true,
+        hintStyle: GoogleFonts.poppins(
+            color: colors.primaryTextColor.withOpacity(0.7)),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(5)),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(5)),
+      ),
+    );
+  }
+}
